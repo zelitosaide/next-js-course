3901a782db6fcdd8e31a0d230aed25f6122d308f
Zelito A. Saide <zelitosaide83@gmail.com>
2023-06-29 00:04:34 +0200

    NextJS

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,9 @@
-"use client"
-
-import { useState } from "react";
+import Link from "next/link";
 
 export default function Home() {
-  const [count, setCount] = useState(0);
-
   return (
-    <main>
-      <button 
-        onClick={function() {
-          setCount(count + 1);
-        }}
-      >
-        Count {count}
-      </button>
-    </main>
+    <h1>
+      Read <Link href="/posts/first-post">this page.</Link>
+    </h1>
   );
 }
---
+++ app/posts/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Posts</h1>;
+}
