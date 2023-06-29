764a033f2f78d772e002fa02d8136f678a2c74f9
Zelito A. Saide <zelitosaide83@gmail.com>
2023-06-29 18:24:15 +0200

    Polishing Layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,9 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
 
+const name = "Zelito Atumane Saide";
+export const siteTitle = "Next.js Sample Website";
+
 export default function Layout({ children, home } : any) {
   const className = styles.container;
 
@@ -8,7 +11,16 @@ export default function Layout({ children, home } : any) {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
       </Head>
       {children}
     </div>
