cd1d9c9a46885646a2c902442881c0c79df27471
Zelito A. Saide <zelitosaide83@gmail.com>
2023-06-29 17:24:02 +0200

    NextJS

---
+++ components/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children } : any) {
+  return <div>{children}</div>;
+}
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import Head from "next/head";
 import Script from "next/script";
+import Layout from "@/components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First post</title>
       </Head>
@@ -19,6 +20,6 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-    </>
+    </Layout>
   );
 }
