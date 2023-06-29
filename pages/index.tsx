92b26c32381ded5f58d3a5494a672b7bcaa752df
Zelito A. Saide <zelitosaide83@gmail.com>
2023-06-29 19:20:43 +0200

    Polishing Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import Layout, { siteTitle } from "@/components/layout";
+import utilStyles from "@/styles/utils.module.css";
+
 export default function Home() {
   return (
-    <>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
+        <title>{siteTitle}</title>
       </Head>
-      <h1>
-        Read <Link href="/posts/first-post">this page.</Link>
-      </h1>
-    </>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hello, I’m Shu. I’m a software engineer and a translator 
+          (English/Portugues). You can contact me on 
+          <a target="_blank" href="https://twitter.com/saide_zelito/">Twitter</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
