a1fb19c1aaf0266df65ca8be63c03036d96f28a5
Zelito A. Saide <zelitosaide83@gmail.com>
2023-06-29 13:53:38 +0200

    NextJS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>
-      Read <Link href="/posts/first-post">this page.</Link>
-    </h1>
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>
+        Read <Link href="/posts/first-post">this page.</Link>
+      </h1>
+    </div>
   );
 }
