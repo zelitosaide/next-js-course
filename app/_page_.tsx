75857d67aa9e91c7e5df5a5e2b074770b7e55eaa
Zelito A. Abdala <42151635+zelitosaide@users.noreply.github.com>
2023-07-08 16:40:33 +0200

    dd

--- app/_page_.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <h1>
-      Read <Link href="/posts/first-post">this page.</Link>
-    </h1>
-  );
-}
--- app/posts/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>Posts</h1>;
-}
