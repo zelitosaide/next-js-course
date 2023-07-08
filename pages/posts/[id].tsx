17052facf5c0e0ded067d7e5f590d4900f953258
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-08 15:35:19 +0200

    NextJS

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,14 @@
 import Layout from "@/components/layout";
+import { getAllPostIds } from "@/lib/posts";
+
+export async function getStaticPaths() {
+  const paths : any = getStaticPaths();
+
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 export default function Post() {
   return <Layout></Layout>;
