62e0a762127659651d2c885860379d5428757e5e
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-09 13:47:30 +0200

    Client Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Logo from './logo'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <Logo />
+        </nav>
+        <div>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
---
+++ app/logo.tsx
@@ -0,0 +1,3 @@
+export default function Logo() {
+  return <h1 className="displa">Logo</h1>;
+}
