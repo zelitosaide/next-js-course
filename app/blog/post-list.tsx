426ee0ae42d4c0494c50f75ae848ff3ad4007b21
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-09 18:55:59 +0200

    Linking and Navigating

--- app/blog/post-list.tsx
+++ app/blog/post-list.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function PostList({ posts } : any) {
   return (
-    <ul className="w-36 bg-green-950 p-2">
+    <ul className="w-48 bg-green-950 p-2">
       {posts.map((post : any) => (
         <li key={post.id}>
           <Link 
             href={`/blog/${post.id}`}
             className="text-sm bg-slate-500 m-1"
           >
-            {post.title}
+            {post.title.slice(0, 20)}...
           </Link>
         </li>
       ))}
