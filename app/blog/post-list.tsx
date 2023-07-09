0869fa1f83dbfee8268fd76e5ff76920a2c331e0
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-09 19:27:04 +0200

    Linking and Navigating

--- app/blog/post-list.tsx
+++ app/blog/post-list.tsx
@@ -1,18 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function PostList({ posts } : any) {
+  const pathname = usePathname();
+
   return (
     <ul className="w-48 shrink-0 bg-green-950 p-2">
-      {posts.map((post : any) => (
-        <li key={post.id}>
-          <Link 
-            href={`/blog/${post.id}`}
-            className="text-sm bg-slate-500 m-1"
-          >
-            {post.title.slice(0, 20)}...
-          </Link>
-        </li>
-      ))}
+      {posts.map((post : any) => {
+        return (
+          <li key={post.id}>
+            <Link 
+              href={`/blog/${post.id}`}
+              className="text-sm bg-slate-500 m-1"
+            >
+              {post.title.slice(0, 20)}...
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
