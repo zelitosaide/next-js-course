f9a6d01bbac6df00ac8495f700169f452496d9f9
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-09 19:37:13 +0200

    Linking and Navigating

--- app/blog/post-list.tsx
+++ app/blog/post-list.tsx
@@ -9,11 +9,13 @@ export default function PostList({ posts } : any) {
   return (
     <ul className="w-48 shrink-0 bg-green-950 p-2">
       {posts.map((post : any) => {
+        const isActive = pathname.split("/")[pathname.split("/").length - 1] == post.id;
+
         return (
           <li key={post.id}>
             <Link 
               href={`/blog/${post.id}`}
-              className="text-sm bg-slate-500 m-1"
+              className={`text-sm bg-slate-500 m-1 ${isActive ? "text-red-400" : ""}`}
             >
               {post.title.slice(0, 20)}...
             </Link>
