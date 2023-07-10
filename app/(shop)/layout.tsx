22114e47d945e32f4a3f7ef549ee70b32f831b89
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 13:07:46 +0200

    Route Groups

---
+++ app/(shop)/layout.tsx
@@ -0,0 +1,10 @@
+export default function AccountLayout({
+  children
+}: { children: React.ReactNode }) {
+  return (
+    <section>
+      <h1>Account Layout</h1>
+      {children}
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <h1>Hello, Home Page!</h1>
       <Link className="block" href="/blog">Blog</Link>
+      <Link className="block" href="/account">Acount</Link>
     </>
   );
 }
