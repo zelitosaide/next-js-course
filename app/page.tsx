dbd134b0b16df51f5484e51127e1613a445135a8
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-10 12:59:30 +0200

    Route Groups

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <>
       <h1>Hello, Home Page!</h1>
-      <Link className="block" href="/dashboard">Dashboard</Link>
       <Link className="block" href="/blog">Blog</Link>
     </>
   );
