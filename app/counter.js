01f47c60ad4bbfa8f07912a4835c3a8338ae0125
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-24 11:51:23 +0200

    Client Components

--- app/counter.js
+++ app/counter.js
@@ -1,11 +1,25 @@
 "use client";
 
-import Button from "./button";
+import { useState } from "react";
 
 export default function Counter() {
   return (
     <div>
       <Button />
     </div>
+  );
+}
+
+export function Button() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button
+      onClick={function() {
+        setCount(count + 1);
+      }}
+    >
+      Click me ( { count } )
+    </button>
   );
 }
