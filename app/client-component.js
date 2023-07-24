bab36baa4d2ee74e051c2ce3ebd7721228f9d737
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-24 13:36:04 +0200

    Composing Client and Server Components

--- app/client-component.js
+++ app/client-component.js
@@ -1,9 +1,11 @@
 "use client";
 
+import SearverComponent from "./server-component";
+
 export default function ClientComponent() {
   return (
     <div className="bg-red">
-
+      <SearverComponent />
     </div>
   );
 }
--- app/server-component.js
+++ app/server-component.js
@@ -1,3 +1,10 @@
 export default async function SearverComponent() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  console.log(response);
   
+  return (
+    <pre>
+      {}
+    </pre>
+  );
 }
