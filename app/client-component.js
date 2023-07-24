c5becaaeed8d4abdd60a6df0fbdce7b43b5f5969
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-24 14:24:06 +0200

    Composing Client and Server Components

--- app/client-component.js
+++ app/client-component.js
@@ -1,11 +1,15 @@
 "use client";
 
+import { useState } from "react";
 import SearverComponent from "./server-component";
 
 export default function ClientComponent() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="bg-red">
       <SearverComponent />
+      <button onClick={function() { setCount(count + 1); }}>click me {count}</button>
     </div>
   );
 }
