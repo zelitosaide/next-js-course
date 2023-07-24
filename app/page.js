eacd8c910254fb44412d1f86468d670e01acf209
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-24 11:08:19 +0200

    Client Components

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import Counter from "./counter";
 
 export default function Home() {
   return (
@@ -102,6 +105,18 @@ export default function Home() {
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+
+        <a
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            <Counter />
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
