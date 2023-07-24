f83904b78c74a05228546f8a8ce212299ea1ee3b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-24 13:15:06 +0200

    Moving Client Components to the Leaves

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import SearchBar from './search-bar';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +13,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <Logo />
+          <SearchBar />
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
+}
+
+function Logo() {
+  return <img src="/next.svg" />;
 }
