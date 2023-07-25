efd85658fd261c4f467d52e5fb64fa2421a1c3e1
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-25 16:43:19 +0200

    Context

--- app/lib/data.js
+++ app/lib/data.js
@@ -6,7 +6,9 @@ export async function getData() {
       authorization: process.env.API_KEY,
     },
   });
+
   const post = await res.json();
+  
   return { 
     data: post, 
     key: process.env.API_KEY, 
