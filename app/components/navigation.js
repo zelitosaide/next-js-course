86cb84d0a9573938d5f595c1e87891ab6b94bec4
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-25 23:25:51 +0200

    Navigation

--- app/components/navigation.js
+++ app/components/navigation.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const posts = [
   { id: 100, title: "First post" },
@@ -11,11 +12,20 @@ const posts = [
 ];
 
 export default function Navigation() {
+  const pathname = usePathname();
+
   return (
     <ul>
       {posts.map(function(post) {
+        const segments = pathname.split("/");
+        const isActive = segments[segments.length - 1] == post.id;
+
         return (
-          <Link className="block" key={post.id} href={`/posts/${post.id}`}>
+          <Link 
+            className={`block ${isActive ? "underline" : ""}`}
+            key={post.id} 
+            href={`/posts/${post.id}`}
+          >
             {post.title}
           </Link>
         );
