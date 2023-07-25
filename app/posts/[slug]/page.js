bafc8215df401d612bf6ce53cd804a14bc4d5c30
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-25 23:37:06 +0200

    useRouter

--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -1,5 +1,21 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Page() {
+  const router = useRouter();
+
   return (
-    <div>Post</div>
+    <div>
+      <button 
+        type="button"
+        onClick={function() {
+          router.back();
+        }}
+      >
+        Back Home
+      </button>
+      <div>Post</div>
+    </div>
   );
 }
--- app/posts/page.js
+++ app/posts/page.js
@@ -1,5 +1,5 @@
 export default function Page() {
   return (
-    <div></div>
+    <div className="bg-yellow">Home</div>
   );
 }
