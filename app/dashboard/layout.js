b6c13ead9ad0b79f5e16566d921e80d9ef10de9f
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-25 22:20:03 +0200

    Layout

---
+++ app/dashboard/layout.js
@@ -0,0 +1,10 @@
+export default function DashboardLayout({ children }) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <nav></nav>
+ 
+      {children}
+    </section>
+  );
+}
