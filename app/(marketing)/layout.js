9848ef9636b94f736a01a022b7c9a526239d22bb
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-27 15:17:29 +0200

    Route Groups

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,3 +1,5 @@
+import "../globals.css";
+
 export default function MarketingLayout({ children }) {
   return (
     <html>
--- app/(shop)/layout.js
+++ app/(shop)/layout.js
@@ -1,3 +1,5 @@
+import "../globals.css";
+
 export default function ShopLayout({ children }) {
   return (
     <html>
