94c43541311f8475a000d83933d66ab6a13ca6e9
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-27 17:08:21 +0200

    Dynamic Routes

--- app/(marketing)/blog/[slug]/page.js
+++ app/(marketing)/blog/[slug]/page.js
@@ -1,7 +1,22 @@
-export default function Page({ params }) {
+export async function generateStaticParams() {
+  const posts = await fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(function(res) { return res.json(); });
+
+  return posts.map(function(post) {
+    return {
+      slug: post.id.toString()
+    };
+  });
+}
+
+export default async function Page({ params }) {
+  const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.slug}`)
+    .then(function(res) { return res.json(); });
+
   return (
     <div>
       My Post: {JSON.stringify(params)}
+      <h1>Title: {post.title}</h1>
     </div>
   );
 }
