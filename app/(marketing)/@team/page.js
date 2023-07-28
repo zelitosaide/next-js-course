f06dfe2a0885740ad204c2b4670780d7fef5ba7b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-28 11:37:56 +0200

    Parallel Routes

---
+++ app/(marketing)/@team/page.js
@@ -0,0 +1,7 @@
+export default async function Page() {
+  return (
+    <div>
+      <h1>Team</h1>
+    </div>
+  );
+}
--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,11 +1,15 @@
 import "../globals.css";
 
-export default function MarketingLayout({ children }) {
+export default function MarketingLayout({ children, analytics, team }) {
+  console.log(analytics);
+
   return (
     <html>
       <body>
         <div>
           <h1>Marketing Layout</h1>
+          <h2>Parallel Routes</h2>
+          {analytics}
           {children}
         </div>
       </body>
