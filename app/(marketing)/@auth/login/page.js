3fdc438dccc263b6346dce332a654d11be4ae514
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-28 18:06:35 +0200

    Parallel Routes

--- app/(marketing)/@auth/login/page.js
+++ app/(marketing)/@auth/login/page.js
@@ -1,7 +1,7 @@
 // "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 export default function Page() {
   // const router = useRouter();
