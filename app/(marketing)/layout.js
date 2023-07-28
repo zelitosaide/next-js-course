e460491fc080125b95bc6c5a7e40d2c50b57c2c2
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-28 14:41:44 +0200

    Parallel Routes

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -1,14 +1,16 @@
 import "../globals.css";
+import { getUser } from "../lib/auth";
 
 export default function MarketingLayout({ children, analytics, team }) {
+  const isLoggedIn = getUser();
+
   return (
     <html>
       <body>
         <div>
           <h1>Marketing Layout</h1>
           <h2>Parallel Routes</h2>
-          {analytics}
-          {team}
+          {isLoggedIn ? analytics : team}
           {children}
         </div>
       </body>
