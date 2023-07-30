1fc131e35953f87216138da9f4ea86c5cf5f4741
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 11:39:46 +0200

    Fetching

--- app/page.js
+++ app/page.js
@@ -1,4 +1,16 @@
-export default function Page() {
+async function getPost() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+  const post = await getPost();
+
   return (
     <div>
       <h1>Home</h1>
