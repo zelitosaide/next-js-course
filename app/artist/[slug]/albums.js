9f3458340f7d9282feb09f94dac35effe5d7adf4
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 18:14:25 +0200

    Fetching

--- app/artist/[slug]/albums.js
+++ app/artist/[slug]/albums.js
@@ -3,15 +3,18 @@ export default async function Albums({ promise }) {
   const albums = await promise;
 
   return (
-    <ul>
-      {albums.map(function(album) {
-        return (
-          <li key={album.id}>
-            <h2>Title: {album.title}</h2>
-            <p>Year: {album.release_year}</p>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <h1>Albums:</h1>
+      <ul>
+        {albums.map(function(album) {
+          return (
+            <li key={album.id}>
+              <h2>Title: {album.title}</h2>
+              <p>Year: {album.release_year}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
