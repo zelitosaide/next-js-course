970ef86e749f89e9228996f13dd1862ef748aaa3
Zelito A. Saide <zelitosaide83@gmail.com>
2023-07-30 14:23:37 +0200

    Fetching

---
+++ app/artist/[slug]/page.js
@@ -0,0 +1,25 @@
+async function getArtist(slug) {
+  const res = await fetch(`http://localhost:3001/artists/${slug}`);
+  return res.json();
+}
+
+async function getArtistAlbums(slug) {
+  const res = await fetch(`http://localhost:3001/artists/${slug}/albums`);
+  return res.json();
+}
+
+export default async function Page({ params: { slug }}) {
+  // Initiate both requests in parallel
+  const artistData = getArtist(slug);
+  const albumsData = getArtistAlbums(slug);
+
+  // Wait for the promises to resolve
+  const [artist, albums] = await Promise.all([artistData, albumsData]);
+
+  return (
+    <>
+      <h1>Name: {artist.name}</h1>
+      <ul></ul>
+    </>
+  );
+}
