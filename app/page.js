517cc9a76511aa10cdc3e33df348595d327a47b4
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-04 19:33:38 +0200

    Fetching

--- app/page.js
+++ app/page.js
@@ -1,27 +1,27 @@
 // import { getDate } from "./utils/api";
 
-import { getFormattedDate, getDate } from "./utils/api";
+// import { getFormattedDate, getDate } from "./utils/api";
 
 // import { db } from "./utils/database";
 
 // export const revalidate = 3600;
 
-async function getDBVersion() {
-  const res = await db.query("SELECT version();");
-  return res;
-}
+// async function getDBVersion() {
+//   const res = await db.query("SELECT version();");
+//   return res;
+// }
 
 export default async function Page() {
   const date = await getDate();
   // const version = await getDBVersion();
-  const formattedDate = await getFormattedDate();
+  // const formattedDate = await getFormattedDate();
 
   return (
     <div>
       <h1>Home</h1>
-      <pre>{JSON.stringify(date)}</pre>
+      {/* <pre>{JSON.stringify(date)}</pre> */}
       {/* <pre>{JSON.stringify(version)}</pre> */}
-      <pre>{JSON.stringify(formattedDate)}</pre>
+      {/* <pre>{JSON.stringify(formattedDate)}</pre> */}
     </div>
   );
 }
