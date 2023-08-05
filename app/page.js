562f306021b02abcc39c42ab4c4e14f9437be23e
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-05 14:09:57 +0200

    Data Fetching

--- app/page.js
+++ app/page.js
@@ -11,6 +11,16 @@
 //   return res;
 // }
 
+async function getData() {
+  const res = fetch("http://localhost:3001");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 export default async function Page() {
   // const date = await getDate();
   // const version = await getDBVersion();
