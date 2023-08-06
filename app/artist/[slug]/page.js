626a164383c003c1c968e6b5382b5cef718f4a9a
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-06 10:40:58 +0200

    Patterns

--- app/artist/[slug]/page.js
+++ app/artist/[slug]/page.js
@@ -23,24 +23,26 @@ async function Playlists({ artistID }) {
 
 export default async function Page({ params: { slug }}) {
   // Initiate both requests in parallel
-  const artistData = getArtist(slug);
-  const albumsData = getArtistAlbums(slug);
+  // const artistData = getArtist(slug);
+  // const albumsData = getArtistAlbums(slug);
 
   // Wait for the promises to resolve
   // const [artist, albums] = await Promise.all([artistData, albumsData]);
 
   // Wait for the artist's promise to resolve first
-  const artist = await artistData
+  // const artist = await artistData
+
+  const artist = await getArtist(slug);
 
   return (
     <>
       <h1>Name: {artist.name}</h1>
       {/* Send the artist information first, and wrap albums in a suspense boundary */}
-      <hr />
+      {/* <hr />
       <Suspense fallback={<div>Loading...</div>}>
-        {/* <Albums list={albums} /> */}
+        <Albums list={albums} />
         <Albums promise={albumsData} />
-      </Suspense>
+      </Suspense> */}
       <hr />
       <Suspense fallback={<div>Loading...</div>}>
         <Playlists artistID={slug} />
