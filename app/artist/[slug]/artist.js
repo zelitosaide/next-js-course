d2eea36998bedf784ca263b3d1c76aad966b262c
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-06 12:36:17 +0200

    Patterns

--- app/artist/[slug]/artist.js
+++ app/artist/[slug]/artist.js
@@ -1,15 +1,19 @@
 import "server-only";
 
 import { cache } from "react";
-import { getArtist } from "@/app/utils/api";
+// import { getArtist } from "@/app/utils/api";
 
 export function preload(slug) {
   void getArtist(slug);
 }
 
+export const getArtist = cache(async function(slug) {
+  const res = await fetch(`http://localhost:3001/artists/${slug}`);
+  return res.json();
+});
+
 export async function Artist({ slug }) {
   const artist = await getArtist(slug);
-  console.log(cache)
 
   return <h1>Name: {artist.name}</h1>;
 }
