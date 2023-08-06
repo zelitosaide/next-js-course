ade24c27f182474338dd94e77ee64c338f605b2b
Zelito A. Saide <zelitosaide83@gmail.com>
2023-08-06 13:41:09 +0200

    Server Actions

--- app/add-to-card.js
+++ app/add-to-card.js
@@ -10,10 +10,16 @@ export default function AddToCard({ productId }) {
     console.log(productId, data);
   }
 
+  async function submitImage(data) {
+    "use server";
+    console.log(productId, data);
+  }
+
   return (
     <form action={addItem}>
       <input type="text" name="name" />
-      <button type="submit">Add to Card</button>
+      <button className="block" type="submit">Add to Card</button>
+      <input type="image" formAction={submitImage} />
     </form>
   );
 }
