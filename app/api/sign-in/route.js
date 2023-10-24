e443de514da307718f4576156adbde7d04371c95
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-24 11:49:01 +0200

    NextJS Course

--- app/api/sign-in/route.js
+++ app/api/sign-in/route.js
@@ -1,8 +1,6 @@
-import { NextResponse } from "next/server";
-
 export async function POST(request) {
   const { email, password } = await request.json();
-  
+
   const res = await fetch("http://localhost:3001/authentication/sign-in", {
     method: "POST",
     headers: {
@@ -14,15 +12,4 @@ export async function POST(request) {
   const data = await res.json();
 
   return Response.json(data);
-
-
-  // const res = await fetch('https://data.mongodb-api.com/...', {
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'API-Key': process.env.DATA_API_KEY,
-  //   },
-  // })
-  // const data = await res.json()
- 
-  // return Response.json({ data })
 }
