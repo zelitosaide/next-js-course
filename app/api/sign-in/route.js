67b1aad944342c4277f645b77eda198c3d667889
Zelito A. Saide <zelitosaide83@gmail.com>
2023-10-24 11:48:12 +0200

    NextJS Course

--- app/api/sign-in/route.js
+++ app/api/sign-in/route.js
@@ -2,13 +2,27 @@ import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const { email, password } = await request.json();
-  const response = await fetch("http://localhost:3001/authentication/sign-in", {
+  
+  const res = await fetch("http://localhost:3001/authentication/sign-in", {
     method: "POST",
-    body: JSON.stringify({ email, password }),
     headers: {
       "Content-type": "application/json; charset=UTF-8",
     },
+    body: JSON.stringify({ email, password }),
   });
-  // const data = await response.json();
-  return NextResponse.json({ data: await response.json() });
+
+  const data = await res.json();
+
+  return Response.json(data);
+
+
+  // const res = await fetch('https://data.mongodb-api.com/...', {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'API-Key': process.env.DATA_API_KEY,
+  //   },
+  // })
+  // const data = await res.json()
+ 
+  // return Response.json({ data })
 }
